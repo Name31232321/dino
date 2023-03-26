53f4ae779fc3abc861496b53d5ba93dcdb9fdfa7
Name31232321 <ywp92@naver.com>
2023-03-26 12:40:42 +0900

    dino ver 1.0

--- index.js
+++ index.js
@@ -4,18 +4,22 @@ var ctx = canvas.getContext('2d');
 canvas.width = window.innerWidth-100;
 canvas.height = window.innerHeight-100;
 
+var img1 = new Image();
+img1.src = './assets/cactus.png';
+
+var img2 = new Image();
+img2.src = './assets/dino.png';
+
 var dino = {
     x:10,
-    y:200,
+    y:230,
     width: 50,
     height: 50,
     draw(){
         ctx.fillStyle='green';
-        ctx.fillRect(this.x,this.y,this.width,this.height)
+        ctx.drawImage(img2, this.x,this.y,this.width,this.height)
     }
 }
-
-
 
 class Cactus {
     constructor(){
@@ -26,11 +30,7 @@ class Cactus {
     }
     draw(){
         ctx.fillStyle='red';
-        ctx.fillRect(this.x,this.y,this.width,this.height);
-        cactusmix.forEach((a)=>{
-            a.x--;
-            a.draw();
-        })
+        ctx.drawImage(img1, this.x,this.y,this.width,this.height);
     }
 }
 
@@ -39,19 +39,61 @@ cactus.draw();
 
 var timer = 0;
 var cactusmix = [];
+var jumpingTime=0;
+var animation;
 
 function actionPerFrame(){
-    requestAnimationFrame(actionPerFrame);
+    animation = requestAnimationFrame(actionPerFrame);
     timer++;
     
     ctx.clearRect(0,0, canvas.width, canvas.height);
+    
     if(timer%120===0){
         var cactus = new Cactus();
         cactusmix.push(cactus);
     }
-    cactus.draw();
-    
+    cactusmix.forEach((a, i, o)=>{
+        if(a.x < 0-a.width ){
+           o.splice(i,1);
+        }
+        a.x--;
+        a.draw();
+
+        collision(dino, a);
+    });
+    if(jumping==true){
+        dino.y-=2;
+        jumpingTime++;
+    }
+
+    if(jumping==false){
+        if(dino.y<230)
+            dino.y+=2;
+    }
+
+    if(jumpingTime > 50){
+        jumping = false;
+        jumpingTime=0;
+    }
+
     dino.draw();
+    
 }
 
-actionPerFrame();
+actionPerFrame(); 
+var jumping = false;
+document.addEventListener('keydown',function(e){
+    if(e.code==='Space'){
+        jumping = true;
+    }
+})
+
+function collision(dino, cactus){
+    var xDifference = cactus.x - (dino.x+dino.width);
+    var yDifference = cactus.y - (dino.y+dino.height);
+
+    if(xDifference < 0 && yDifference < 0){
+        ctx.clearRect(0,0,canvas.width,canvas.height);
+        cancelAnimationFrame(animation);
+    };
+}
